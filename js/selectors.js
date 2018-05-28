8de09df03e7b4753bf08a6a5d0edfbeaa7e618f8
Christopher Stull <chris.stull@gmail.com>
2018-05-28 20:41:41 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('img:last')
+}
+
+function ninjaBabySelector(){
+  return $('img[id="ninja-baby"]')
+}
+
+function divSelector(){
+  return $("div[class='pics']")
+}
+
+function firstListItem(){
+  return $("ul li[id='pic-list']:first-child")
+}
